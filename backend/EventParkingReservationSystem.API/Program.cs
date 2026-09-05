fa811017c536894be8e18525ba8403cba5a4ae1f
Thulaanchan <kthulaanchan@gmail.com>
2026-09-05 15:32:04 +0530

    register notification dependencies

--- backend/EventParkingReservationSystem.API/Program.cs
+++ backend/EventParkingReservationSystem.API/Program.cs
@@ -1,5 +1,6 @@
 using EventParkingReservationSystem.API.Data.Context;
 using Microsoft.EntityFrameworkCore;
+
 using EventParkingReservationSystem.API.Interfaces.Repositories.Customers;
 using EventParkingReservationSystem.API.Repositories.Customers;
 using Microsoft.AspNetCore.Identity;
@@ -7,18 +8,36 @@ using EventParkingReservationSystem.API.Models.Entities.Customers;
 using EventParkingReservationSystem.API.Interfaces.Services.Customers;
 using EventParkingReservationSystem.API.Services.Customers;
 
+using EventParkingReservationSystem.API.Interfaces.Repositories.Notifications;
+using EventParkingReservationSystem.API.Repositories.Notifications;
+using EventParkingReservationSystem.API.Interfaces.Services.Notifications;
+using EventParkingReservationSystem.API.Services.Notifications;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database connection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+// Customer dependencies
+builder.Services.AddScoped<ICustomerRepository,
+    CustomerRepository>();
+
 builder.Services.AddScoped<IPasswordHasher<Customer>,
     PasswordHasher<Customer>>();
 
 builder.Services.AddScoped<ICustomerService,
     CustomerService>();
 
-// Add services to the container.
+// Notification dependencies
+builder.Services.AddScoped<INotificationRepository,
+    NotificationRepository>();
+
+builder.Services.AddScoped<INotificationService,
+    NotificationService>();
+
+// Add services to the container
 builder.Services.AddControllers();
 
 // Swagger / OpenAPI
@@ -26,7 +45,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
