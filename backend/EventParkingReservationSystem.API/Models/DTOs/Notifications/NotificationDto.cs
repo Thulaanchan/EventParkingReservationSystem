f92dba8a496b6c380a03c7dc736728c48649633c
Thulaanchan <kthulaanchan@gmail.com>
2026-09-05 13:33:11 +0530

    added notification dto

---
+++ backend/EventParkingReservationSystem.API/Models/DTOs/Notifications/NotificationDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EventParkingReservationSystem.API.Models.DTOs.Notifications
+{
+    public class NotificationDto
+    {
+        public int NotificationId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; }
+    }
+}
