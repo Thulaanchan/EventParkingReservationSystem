956d7ec2c1d40b2adc5f06bd1ce1d5c60b7861c6
Thulaanchan <kthulaanchan@gmail.com>
2026-09-05 14:37:52 +0530

    added notification service interface

---
+++ backend/EventParkingReservationSystem.API/Interfaces/Services/Notifications/INotificationService.cs
@@ -0,0 +1,22 @@
+﻿using EventParkingReservationSystem.API.Models.DTOs.Notifications;
+
+namespace EventParkingReservationSystem.API.Interfaces.Services.Notifications
+{
+    public interface INotificationService
+    {
+        Task<IEnumerable<NotificationDto>> GetCustomerNotificationsAsync(
+            int customerId);
+
+        Task<int> GetUnreadCountAsync(
+            int customerId);
+
+        Task<bool> MarkAsReadAsync(
+            int notificationId,
+            int customerId);
+
+        Task CreateNotificationAsync(
+            int customerId,
+            string title,
+            string message);
+    }
+}
