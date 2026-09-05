eba2c1098414012d6a5725cdde7a0a4d86f05dee
Aariyanka13 <aariyanka13@gmail.com>
2026-09-05 12:51:36 +0530

    add authentication DTO foundation

--- backend/EventParkingReservationSystem.API/Models/DTOs/Auth/ResetPasswordRequestDto.cs
+++ backend/EventParkingReservationSystem.API/Models/DTOs/Auth/ResetPasswordRequestDto.cs
@@ -1,6 +1,22 @@
-﻿namespace EventParkingReservationSystem.API.Models.DTOs.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventParkingReservationSystem.API.Models.DTOs.Auth;
+
+public class ResetPasswordRequestDto
 {
-    public class ResetPasswordRequestDto
-    {
-    }
+    [Required]
+    [EmailAddress]
+    [MaxLength(150)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Token { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(8)]
+    public string NewPassword { get; set; } = string.Empty;
+
+    [Required]
+    [Compare(nameof(NewPassword))]
+    public string ConfirmNewPassword { get; set; } = string.Empty;
 }
